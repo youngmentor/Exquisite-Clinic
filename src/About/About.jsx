bb0dcd95cfc5b4143810ee91f16bc9bf904a50d7
youngmentor <olaleresuliton43@gmail.com>
2023-03-17 00:51:01 -0700

    Added about page

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import "./About.css"
 const About = () => {
-  return (
-    <div>
-        <h2>About Us</h2>
-    </div>
-  )
+    return (
+        <div className='About_Main'>
+            <h2>About Us</h2>
+            <div className='About_Main_Wrap'>
+                <p>Exquisite Physiotherapy Clinic specialists help restore movement and function to people limited by disease or injury.
+                    Physiotherapist diagnose and treat a variety of medical conditions affecting the brain, spinal cord, nerves and the musculoskeletal system. They provide treatment and rehabilitation options that reflect the unique needs, abilities and objectives of each patient.
+                    Restoring function simply means helping you regain the best options for your quality of life, retain as much muscle ability as possible, help you re-train mental pathways, and generally reach your best possible capability given your specific needs.</p>
+            </div>
+        </div>
+    )
 }
 
 export default About
