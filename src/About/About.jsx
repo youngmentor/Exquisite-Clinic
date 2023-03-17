b437c1291b9cc434cdb5ffcec78fdc2852971721
youngmentor <olaleresuliton43@gmail.com>
2023-03-17 08:59:56 -0700

    added mission

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -8,6 +8,15 @@ const About = () => {
                 <p>Exquisite Physiotherapy Clinic specialists help restore movement and function to people limited by disease or injury.
                     Physiotherapist diagnose and treat a variety of medical conditions affecting the brain, spinal cord, nerves and the musculoskeletal system. They provide treatment and rehabilitation options that reflect the unique needs, abilities and objectives of each patient.
                     Restoring function simply means helping you regain the best options for your quality of life, retain as much muscle ability as possible, help you re-train mental pathways, and generally reach your best possible capability given your specific needs.</p>
+
+              <div className='About_List'>
+              <h4>WE ACHIEVE OUR GOALS THROUGH.</h4>
+                <ul className='List'>
+                    <li>HOSPITAL BASED TREATMENT</li>
+                    <li>HOME CARE SERVICE</li>
+                    <li>TELEPHYSIOTHERAPY</li>
+                </ul>
+              </div>
             </div>
         </div>
     )
--- src/Mission/Mission.jsx
+++ src/Mission/Mission.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 import "./Mission.css"
 const Mission = () => {
   return (
-    <div>
-        our mission and vision
+    <div className='Mission_Main'>
+        <div className='Mission_Main_Wrap'>
+            <h2>Our Mission And Vision</h2>
+            <div className='Mission_Wrap'>
+                <div className='Our_Mission'>
+                    <h4>Our Mission</h4>
+                    <p>Inspiring hope and adding maximum motion back to life  through integrated clinical practice, education and research.</p>
+                </div>
+                <div className='Our_Vision'>
+                  <h4>Our Vision</h4>
+                  <p>Transforming Physiotherapy to connect and cure as the global authority in the Rehabilitation of clients with  serious or complex disease.</p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
