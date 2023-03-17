d3ccfc1b1883008063356b4455486f94e6c5cb50
youngmentor <olaleresuliton43@gmail.com>
2023-03-16 23:32:40 -0700

    BUILDING HEADER

---
+++ src/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    <div className='Header_Main'>
+        <img src='/123.png' alt='' />
+    </div>
+  )
+}
+
+export default Header
