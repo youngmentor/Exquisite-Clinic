613b8b0b2c820684cced4fdc4367f8cb848c5672
youngmentor <olaleresuliton43@gmail.com>
2023-03-19 21:05:13 -0700

    service page

--- src/OurService/Service.jsx
+++ src/OurService/Service.jsx
@@ -3,7 +3,10 @@ import "./Service.css"
 const Service = () => {
   return (
     <div className='Service_Main'>
- <h1>our service</h1>
+      <div className='Service_Main_Wrap'>
+      <h1>our service</h1>
+      <h3>We provide exquisite physiotherapy services in the following area:</h3>
+      </div>
     </div>
   )
 }
