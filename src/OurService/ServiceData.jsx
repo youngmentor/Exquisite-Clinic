2a42ba0a695fc38a8d82680ee41f63c5a0d85ea4
youngmentor <olaleresuliton43@gmail.com>
2023-03-19 19:59:16 -0700

    added services

---
+++ src/OurService/ServiceData.jsx
@@ -0,0 +1,65 @@
+
+
+const Services = [
+    {
+        id: 1,
+        img: "",
+        txt: "",
+    },
+    {
+        id: 2,
+        img: "",
+        txt: "",
+    },
+    {
+        id: 3,
+        img: "",
+        txt: "",
+    },
+    {
+        id: 4,
+        img: "",
+        txt: "",
+    },
+    {
+        id: 5,
+        img: "",
+        txt: "",
+    },
+    {
+        id: 6,
+        img: "",
+        txt: "",
+    },
+    {
+        id: 7,
+        img: "",
+        txt: "",
+    },
+    {
+        id: 8,
+        img: "",
+        txt: "",
+    },
+    {
+        id: 9,
+        img: "",
+        txt: "",
+    },
+    {
+        id: 10,
+        img: "",
+        txt: "",
+    },
+    {
+        id: 11,
+        img: "",
+        txt: "",
+    },
+    {
+        id: 12,
+        img: "",
+        txt: "",
+    },
+]
+export default Services
